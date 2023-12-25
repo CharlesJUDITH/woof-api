55aa818239f280f147623b807643341ceb445a71
Charles JUDITH <contact@charlesjudith.com>
2023-12-25 17:57:25 +0100

    Add images to make it more funny

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import random
 
 app = Flask(__name__)
@@ -13,16 +13,18 @@ chihuahua_facts = [
     "They were once believed to have healing powers and were used in religious rituals by ancient civilizations."
 ]
 
+chihuahua_images = [
+    "/static/images/chihuahua1.png",
+    "/static/images/chihuahua2.png",
+    "/static/images/chihuahua3.png",
+    "/static/images/chihuahua4.png",
+]
+
 @app.route('/woof')
-def dog():
+def woof():
+    image = random.choice(chihuahua_images)
     fact = random.choice(chihuahua_facts)
-    return f"""
-    <div style='text-align: center;'>
-        <h1>Woof! 🐕</h1>
-        <p><h2>Did you know?</h2></p>
-        <p>{fact}</p>
-    </div>
-    """
+    return render_template("index.html", image_url=image, fact=fact)
 
 if __name__ == '__main__':
     app.run(debug=False)
